a9fa5016b76dc90dc351ba395e3ffb0968931d94
Luis Gustavo Paz Barbosa <138590403+lgGustavodev@users.noreply.github.com>
2025-06-24 20:02:17 -0300

    projeto pronto

--- src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
+++ src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
@@ -3,6 +3,9 @@ package br.com.alura.TabelaFipe.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
@@ -17,5 +20,17 @@ public class ConverteDados implements IConverteDados {
             throw new RuntimeException(e);
         }
         return null;
+    }
+
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class, classe);
+
+        try {
+            return mapper.readValue(json, lista);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
